8b31a593f2866ff988be76918ffe5c571696bf72
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2021-11-11 19:25:12 +0900

    プロジェクトを新規作成

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace MultiPlatformMpegStudio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace MultiPlatformMpegStudio
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            return builder.Build();
+        }
+    }
+}
